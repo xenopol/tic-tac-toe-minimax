68e729a8a67eca2404890c6876b2f139f503b2e3
xenopol <x.enopol@yahoo.com>
2020-12-22 15:19:41 +0000

    Refactor react-native game logic

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {
   Alert,
   SafeAreaView,
@@ -9,41 +9,40 @@ import {
   Pressable,
 } from 'react-native'
 
-// const initialBoard = Array(9).fill(undefined)
-const initialBoard = [
-  'O',
-  'X',
-  'O',
-  'X',
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-]
-const areEqual = (a: string, b: string, c: string) => a && a === b && b === c
-
-const getWinnerCells = (board: string[]) => {
-  if (areEqual(board[0], board[1], board[2])) return [0, 1, 2]
-  if (areEqual(board[3], board[4], board[5])) return [3, 4, 5]
-  if (areEqual(board[6], board[7], board[8])) return [6, 7, 8]
-  if (areEqual(board[0], board[3], board[6])) return [0, 3, 6]
-  if (areEqual(board[1], board[4], board[7])) return [1, 4, 7]
-  if (areEqual(board[2], board[5], board[8])) return [2, 5, 8]
-  if (areEqual(board[0], board[4], board[8])) return [0, 4, 8]
-  if (areEqual(board[2], board[4], board[6])) return [2, 4, 6]
-  return []
+enum Player {
+  Human = 'x',
+  Computer = 'o',
 }
+type Cell = Player | undefined
+type CellId = number
 
-const updateBoard = (cellId: number, isX: boolean, board: string[]) =>
+const initialBoard = Array(9).fill(undefined)
+const initialPlayer = Player.Human
+
+const getWinnerCellIds = (board: Cell[]) =>
+  [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ].find(
+    ([a, b, c]) => board[a] && board[a] === board[b] && board[b] === board[c],
+  ) || []
+
+const isDraw = (board: Cell[]) => board.filter(Boolean).length === 9
+
+const updateBoard = (cellId: CellId, player: Player, board: Cell[]) =>
   board.map((cell, i) => {
-    if (i === cellId) return isX ? 'X' : 'O'
+    if (cellId === i) return player
     return cell
   })
 
-const createAlert = (status: number, resetGame: () => void) => {
-  const title =
-    status > 0 ? 'Player X won!' : status < 0 ? 'Player O won!' : "It's a draw!"
+const createAlert = (player: Player | null, resetGame: () => void) => {
+  const title = player ? `Player ${player} won!` : "It's a draw!"
 
   Alert.alert(title, '', [
     {
@@ -53,110 +52,39 @@ const createAlert = (status: number, resetGame: () => void) => {
   ])
 }
 
-const getEmptyCellIds = (board: string[]) =>
-  board.reduce((acc, cur, i) => (!cur ? [...acc, i] : acc), [] as number[])
-
-const minimax = (board: string[], depth: number, isMax: boolean): number => {
-  const emptyCellIds = getEmptyCellIds(board)
-  if (getWinnerCells(board).length) return isMax ? 1 : -1
-  if (!emptyCellIds.length) return 0
-
-  // maximizer
-  if (isMax) {
-    let bestScore = -Infinity
-    emptyCellIds.forEach((id) => {
-      const score = minimax(updateBoard(id, false, board), depth + 1, !isMax)
-      bestScore = Math.max(score, bestScore)
-    })
-    return bestScore
-  }
-
-  // minimizer
-  let bestScore = Infinity
-  emptyCellIds.forEach((id) => {
-    const score = minimax(updateBoard(id, true, board), depth + 1, !isMax)
-    bestScore = Math.min(score, bestScore)
-  })
-  return bestScore
-}
-
-const getAiMove = (board: string[]): number => {
-  debugger
-  const emptyCellIds = getEmptyCellIds(board)
-  let bestScore = -Infinity
-  let move = 0
-
-  emptyCellIds.forEach((id) => {
-    const score = minimax(board, 0, false)
-    console.log('score', score)
-    if (score > bestScore) {
-      bestScore = score
-      move = id
-    }
-  })
-
-  console.log('move', move)
-
-  return move
-}
+const togglePlayer = (player: Player) =>
+  player === Player.Computer ? Player.Human : Player.Computer
 
 const Board = () => {
-  const [board, setBoard] = useState<string[]>(initialBoard)
-  const [winnerCells, setWinnerCells] = useState<number[]>([])
-  const [isX, setIsX] = useState(true)
-  useEffect(() => {
-    const emptyCells = getEmptyCellIds(board)
-    if (
-      emptyCells.length < 9 &&
-      emptyCells.length > 0 &&
-      !isX &&
-      !winnerCells.length
-    ) {
-      const aiMove = getAiMove(board)
-      makeMove(aiMove)
-    }
-  })
+  const [board, setBoard] = useState<Cell[]>(initialBoard)
+  const [player, setPlayer] = useState<Player>(initialPlayer)
+  const winnerCellIds = getWinnerCellIds(board)
+  const winner = board[winnerCellIds[0]]
 
-  const resetGame = () => {
+  const play = (cellId: CellId) => {
+    const cellOccupied = board[cellId]
+    if (cellOccupied) return
+
+    setBoard(updateBoard(cellId, player, board))
+    setPlayer(togglePlayer)
+  }
+
+  const reset = () => {
     setBoard(initialBoard)
-    setWinnerCells([])
-    setIsX(true)
+    setPlayer(initialPlayer)
   }
 
-  const makeMove = (cellId: number) => {
-    if (board[cellId]) return
-
-    const updatedBoard = updateBoard(cellId, isX, board)
-    const updatedWinnerCells = getWinnerCells(updatedBoard)
-    setBoard(updatedBoard)
-
-    if (updatedWinnerCells.length) {
-      createAlert(isX ? 1 : -1, resetGame)
-      setWinnerCells(updatedWinnerCells)
-
-      return
-    }
-
-    const isDraw = updatedBoard.filter(Boolean).length === 9
-    if (isDraw) {
-      createAlert(0, resetGame)
-
-      return
-    }
-
-    setIsX((current) => !current)
-  }
+  if (winner) createAlert(winner, reset)
+  if (isDraw(board)) createAlert(null, reset)
 
   return (
     <View style={styles.board}>
       {board.map((cell, i) => (
         <Pressable
           key={i}
-          style={[styles.cell, winnerCells.includes(i) && styles.winnerCell]}
-          onPress={() => makeMove(i)}>
-          <Text style={[styles.cellText, cell === 'X' && styles.green]}>
-            {cell}
-          </Text>
+          style={[styles.cell, winnerCellIds.includes(i) && styles.winnerCell]}
+          onPress={() => play(i)}>
+          <Text style={styles.cellText}>{cell}</Text>
         </Pressable>
       ))}
     </View>
@@ -200,10 +128,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   winnerCell: {
-    backgroundColor: 'blue',
+    backgroundColor: '#a5d6a7',
   },
   cellText: {
-    color: 'yellow',
+    fontWeight: 'bold',
     fontSize: 36,
   },
   green: {
